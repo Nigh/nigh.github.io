252cf3d2e5c8bcf46fe73179930b07f4d2e9c7f5
xianii <xianyi.xia@outlook.com>
2025-04-19 04:02:45 +0800

    init astro branch

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+    description: z.string().optional(),
+  }),
+});
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+	url: z.string(),
+	star: z.number().optional(),
+	image: z.string().optional(),
+    draft: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+    description: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  posts, projects
+};
