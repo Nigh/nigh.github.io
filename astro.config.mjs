ae88e39bbe8f5c6cab550a268929d5599bdd0505
xianii <xianyi.xia@outlook.com>
2025-05-30 11:49:37 +0800

    add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 import dotenv from 'dotenv';
 import icon from 'astro-icon';
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), icon()],
+  integrations: [svelte(), icon(), mdx()],
   base: process.env.PUBLIC_BASE_URL || '/',
   site: process.env.PUBLIC_SITE_URL
     ? process.env.PUBLIC_SITE_URL + (process.env.PUBLIC_BASE_URL || '')
