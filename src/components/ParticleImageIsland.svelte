ded96567c72b3ac0d611381204fb66b4d01fb979
xianii <xianyi.xia@outlook.com>
2025-06-24 15:52:07 +0800

    new particle system on index

---
+++ src/components/ParticleImageIsland.svelte
@@ -0,0 +1,16 @@
+<script>
+  import DotsImage from './DotsImage.svelte';
+  export let src = '';
+  const isValidPixel = (rgba) => rgba.a > 50;
+</script>
+
+<DotsImage
+  {src}
+  color="#fff8"
+  width={400}
+  height={400}
+  dotSize={4}
+  gap={1}
+  float={false}
+  {isValidPixel}
+/>
