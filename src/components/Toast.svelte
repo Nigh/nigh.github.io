48161032006c2d43602b661916aa9fd38a0b836c
xianii <xianyi.xia@outlook.com>
2025-06-25 15:44:23 +0800

    opt friend link

---
+++ src/components/Toast.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { toast } from '../stores/toast.ts';
+  import { fly, fade } from 'svelte/transition';
+  $: show = $toast.show;
+  $: message = $toast.message;
+  $: type = $toast.type;
+</script>
+
+{#if show}
+  <div
+    class="toast z-50 fixed"
+    transition:fade={{ duration: 350 }}
+  >
+    <div
+      class="alert"
+      class:alert-success={type === 'success'}
+      class:alert-error={type === 'error'}
+      class:alert-info={type === 'info'}
+    >
+      <span>{message}</span>
+    </div>
+  </div>
+{/if}
